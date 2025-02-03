360b45bd0c8eaadbbe2aeedc7e4466a4101b1f32
saurav1729 <22053276@kiit.ac.in>
2025-02-04 02:59:22 +0530

    fixes email processing

--- controllers/process_order.py
+++ controllers/process_order.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, request, jsonify
-import json
+from flask import jsonify, request
+from models.Connection import Connection
 from app.model import generate_response
+from utils.db import db
+import json
 
-api = Blueprint('api', __name__)
-
-@api.route('/process-order', methods=['POST'])
 def process_order():
     try:
         data = request.get_json()
